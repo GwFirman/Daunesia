4b872cf99bf63aae8d264af6e215dd6c740a8a52
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-07-05 13:18:09 +0700

    feat: add Echo framework and configure basic server with static file serving

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("hehe")
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:   "./client/dist",
+		Browse: false,
+	}))
+
+	e.GET("/*", func(c echo.Context) error {
+		return c.File("./client/dist/index.html")
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
